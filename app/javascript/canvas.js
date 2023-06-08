9c6b6140d50ecaeb3d357cf4b3d48fdf32489060
Yuya7124 <86750113+Yuya7124@users.noreply.github.com>
2023-06-08 23:52:05 +0900

    メーター仮設定

---
+++ app/javascript/canvas.js
@@ -0,0 +1,30 @@
+function canvas_1 () {
+  let canvas = document.getElementById("canvas");
+  let ctx = canvas.getContext("2d");
+  
+  const size = 50;  // 描画範囲の横幅
+  let angle = 0;    // 角度
+  let add = 2;      // 角度に加算する値
+  setInterval(function(){
+    let endAngle = angle * Math.PI / 180;
+    ctx.clearRect(0, 0, size, size);
+    ctx.beginPath();
+    ctx.arc(size / 2, size / 2, size, 0, endAngle, false);
+    ctx.lineTo(size / 2,size / 2);
+    ctx.fillStyle = color_change(120, 200, 255);
+    ctx.fill();
+  
+    angle += add;  // 角度の加算
+  
+    // ゲージが360度または0度になった時に加算数値を切り替える
+    if(angle >= 360 || angle <= 0){
+      add *= -1;
+    }
+  },10);
+}
+
+function color_change (red, green, blue) {
+  return "rgb(" + red + "," + green + "," + blue + ")";
+}
+
+window.addEventListener("load", canvas_1);
