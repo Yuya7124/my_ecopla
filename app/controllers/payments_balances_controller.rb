7d651c37784fd1fc624ee515af92117a799ccc22
Yuya7124 <86750113+Yuya7124@users.noreply.github.com>
2023-07-13 00:45:54 +0900

    一括更新機能実装中

--- app/controllers/payments_balances_controller.rb
+++ app/controllers/payments_balances_controller.rb
@@ -1,5 +1,6 @@
 class PaymentsBalancesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_payments_balance, only: [:edit, :show, :update, :destroy]
 
   def index
     @payments_balances = PaymentsBalance.where(user_id: current_user.id)
@@ -10,7 +11,7 @@ class PaymentsBalancesController < ApplicationController
   end
 
   def create
-    @form = Form::PaymentsBalanceCollection.new(payments_balance_params)
+    @form = Form::PaymentsBalanceCollection.new(payments_balance_collection_params)
     if @form.save
       redirect_to root_path
     else
@@ -18,17 +19,49 @@ class PaymentsBalancesController < ApplicationController
     end
   end
 
-  def show
-    @selected_date = Date.parse(params[:date])
-    @payments_balance = PaymentsBalance.where(date: @selected_date, user_id: current_user.id)
+  def edit
   end
+
+  def update
+    if @payments_balance.update(payments_balances_params)
+      redirect_to payments_balances_path(date: @selected_date)
+    else
+      render :edit
+    end
+  end
+  
+  def destroy 
+    date = @payments_balance.date
+    @payments_balance.destroy
+    redirect_to payments_balances_path(date: date)
+  end
+
+  def show 
+  end
+  
+  def update_multiple
+    binding.pry
+    @payments_balance.each do |balance|
+      balance.update(payments_balances_params)
+    end
+    redirect_to payments_balances_path(date: @selected_date)
+  end 
   
   private
   
-  def payments_balance_params
+  def payments_balance_collection_params
     params.require(:form_payments_balance_collection)
-    .permit(:date, payments_balances_attributes: [:amount, :purpose, :payment_id, :payment_times])
+          .permit(:date, payments_balances_attributes: [:amount, :purpose, :payment_id, :payment_times])
     .merge(user_id: current_user.id)
   end
-  
+
+  def payments_balances_params
+    params.require(:payments_balance)
+          .permit(payments_balances_attributes: [:id, :amount, :purpose, :payment_id, :payment_times, :_destroy])
+  end
+
+  def set_payments_balance
+    @selected_date = Date.parse(params[:date])
+    @payments_balance = PaymentsBalance.where(date: @selected_date, user_id: current_user.id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   get 'payments_balances/index'
   root to: "payments_balances#index"
-  resources :payments_balances, format: false do
+  resources :payments_balances, except: :update, format: false do
     collection do
       get :show, format: false
+      get :edit, format: false
+      patch :update_multiple, format: false
     end
   end
 end
